8aecd867bd1bdc07a8db337886952f3b3b0676d5
manel <manelzaum@icloud.com>
2026-04-18 20:29:19 -0300

    add: parser expression.

--- transpiler/parser.c
+++ transpiler/parser.c
@@ -110,17 +110,18 @@ AST * create_identifier(Token token)
 AST* parse_factor(Parser* p) {
     Token token = parser_peek(p, 0);
 
+    advance_parser(p, 1);
+
     if (token.type == TOKEN_NUMBER) return create_number(token);
     if (token.type == TOKEN_IDENTIFIER) return create_identifier(token);
 
-    printf("Erro: esperado número ou identificador\n");
+    fprintf(stderr, "Erro: esperado número ou variável, mas encontrou '%s'\n", token.text);
     exit(1);
 }
 
 AST * parse_expression(Parser *p)
 {
     AST* left = parse_factor(p);
-    advance_parser(p, 1);
 
     while (1) {
         Token t = parser_peek(p, 0);
@@ -132,45 +133,15 @@ AST * parse_expression(Parser *p)
 
             left = create_operator(t, left, right);
         } else { break; }
-        advance_parser(p, 1);
     }
-
     return left;
-}
-
-AST * parse_assignment(Parser *p)
-{
-    Token to = parser_peek(p, -1);
-    Token sign = parser_peek(p, 0);
-
-    AST *node = malloc(sizeof(AST));
-    node->type = AST_ASSIGN;
-    node->assign.assignment = sign.text;
-    node->assign.to = create_identifier(to);
-
-    advance_parser(p, 1);
-    node->assign.value = parse_expression(p);
-
-    return node;
-}
-
-AST * parse_statement(Parser *p)
-{
-    while (1) {
-        Token token = parser_peek(p, 0);
-
-        if (token.type == TOKEN_EOF) return NULL;
-        if (token.type == TOKEN_EQUALS) return parse_assignment(p);
-
-        advance_parser(p, 1);
-    }
 }
 
 AST * parse(TokenList list)
 {
     Parser p = { list, 0 };
 
-    AST* ast = parse_statement(&p);
+    AST* ast = parse_expression(&p);
 
     Token t = parser_peek(&p, 0);
     if (t.type != TOKEN_EOF) {
