1d12f06d8268c31701a80a45cfcfbf8a812d4dbc
manel <manelzaum@icloud.com>
2026-04-18 15:00:27 -0300

    cleaning tests.

--- transpiler/main.c
+++ transpiler/main.c
@@ -1,30 +1,7 @@
 #include "lexer.h"
 #include "parser.h"
 #include <stdio.h>
-#include <stdlib.h>
 
-int resolve(AST *ast)
-{
-    if (ast == NULL) {
-        printf("Erro: AST nula\n");
-        exit(1);
-    }
-
-    if (ast->type == AST_NUMBER) return ast->number;
-
-    int left = resolve(ast->op.left);
-    int right = resolve(ast->op.right);
-
-    switch (ast->op.sign) {
-        case '+': return left + right;
-        case '-': return left - right;
-        case '*': return left * right;
-        case '/': return left / right;
-    }
-
-    printf("Erro: tipo desconhecido\n");
-    exit(1);
-}
 
 int main(int argc, char *argv[])
 {
@@ -36,10 +13,6 @@ int main(int argc, char *argv[])
 
     AST *ast = parse(tokens);
     show_ast(ast, 0);
-
-    printf("\n");
-
-    printf("Resultado: %d\n", resolve(ast));
 
     return 0;
 }
