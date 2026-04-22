4c34b37ecbff8c5b9b1f659ee466460a852797ea
manel <manelzaum@icloud.com>
2026-04-22 18:21:40 -0300

    add: generating all actual supported code.

--- transpiler/transpiler.c
+++ transpiler/transpiler.c
@@ -32,7 +32,6 @@ char * generate_code(AST *ast)
 {
     char *result = strdup("");
     char *temp = strdup("");
-    char *params = strdup("");
 
     switch (ast->type) {
         case AST_BLOCK:
@@ -41,7 +40,8 @@ char * generate_code(AST *ast)
             }
             sprintf(result, ast->block.main ? "%s" : "{ %s }", temp);
             break;
-        case AST_FUNC_DEF:
+        case AST_FUNC_DEF: {
+            char *params = strdup("");
             for (AST *p = ast->func_def.params; p != NULL; p = p->func_def_param.next) {
                 char *param_code = generate_code(p);
                 if (strcmp(params, "") == 0) sprintf(params, "%s", param_code);
@@ -51,48 +51,72 @@ char * generate_code(AST *ast)
             sprintf(result, "%s %s (%s) %s", ast->func_def.return_type,
                     ast->func_def.name, params, generate_code(ast->func_def.body));
             break;
-
+        }
         case AST_FUNC_DEF_PARAM:
-            sprintf(result, "%s %s", ast->func_def_param.type, ast->func_def_param.name);
+            sprintf(result, "%s %s", ast->func_def_param.type,
+                    ast->func_def_param.name);
             break;
+
         case AST_FUNC_RETURN:
             sprintf(result, "return %s;", generate_code(ast->func_return));
             break;
+
         case AST_OPERATOR:
-            sprintf(result, "%s %c %s", generate_code(ast->op.left), ast->op.sign, generate_code(ast->op.right));
+            sprintf(result, "%s %c %s", generate_code(ast->op.left), ast->op.sign,
+                    generate_code(ast->op.right));
             break;
+
         case AST_IDENTIFIER:
             sprintf(result, "%s", ast->identifier);
             break;
+
         case AST_NUMBER:
             sprintf(result, "%d", ast->number);
             break;
-    }
+        case AST_VAR_DEF:
+            sprintf(result, "%s %s", ast->field.type, ast->field.name);
+          break;
+        case AST_CONST_DEF:
+            sprintf(result, "const %s %s", ast->field.type, ast->field.name);
+          break;
+        case AST_ASSIGN:
+            sprintf(result, "%s %s %s", generate_code(ast->assign.to),
+                    ast->assign.assignment, generate_code(ast->assign.value));
+          break;
+        case AST_FUNC_EXEC: {
+            char *params = strdup("");
+            AST *p = ast->func_exec.params;
+            int first = 1;
+            while (p != NULL) {
+                char *exec_param_code = generate_code(p->func_exec_param.expression);
+                if (first) {
+                    sprintf(params, "%s", exec_param_code);
+                    first = 0;
+                    continue;
+                }
+                sprintf(params, "%s, %s", params, exec_param_code);
+                p = p->func_exec_param.next;
+            }
+            sprintf(result, "%s(%s)", ast->func_exec.name, params);
+          break;
+        }
+        case AST_FUNC_EXEC_PARAM:
+          break;
+        }
 
     return result;
 }
 
 int main(int argc, char *argv[])
 {
-    // if (argc < 2) {
-    //     printf("./transpiler file.nell");
-    //     return 1;
-    // }
-    //
-    // char *source = read_all_file(argv[1]);
-    //
-    // if (source == NULL) return 1;
-    //
-    // TokenList tokens = tokenize(source);
-    //
-    // show_token_list(tokens);
-    //
-    // printf("\n");
-    //
-    // AST *ast = parse(tokens);
-    // show_ast(ast, 0);
+    if (argc < 2) {
+        printf("./transpiler file.nell");
+        return 1;
+    }
 
-    char *source = "soma :: (a: int, b: int) -> int { return a + b; }";
+    char *source = read_all_file(argv[1]);
+
+    if (source == NULL) return 1;
 
     TokenList tokens = tokenize(source);
 
