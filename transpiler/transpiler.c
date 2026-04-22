72711cf08529fce48168ab352e15ca3bd57c729a
manel <manelzaum@icloud.com>
2026-04-22 16:46:16 -0300

    add: prototype of code translation.

--- transpiler/transpiler.c
+++ transpiler/transpiler.c
@@ -1,6 +1,6 @@
 #include "lexer.h"
 #include "parser.h"
-#include <_stdio.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -28,16 +28,71 @@ char * read_all_file(char *path) {
     return content;
 }
 
-int main(int argc, char *argv[])
+char * generate_code(AST *ast)
 {
-    if (argc < 2) {
-        printf("./transpiler file.nell");
-        return 1;
+    char *result = strdup("");
+    char *temp = strdup("");
+    char *params = strdup("");
+
+    switch (ast->type) {
+        case AST_BLOCK:
+            for (int i = 0; i < ast->block.count; i++) {
+                 sprintf(temp, "%s%s", temp, generate_code(ast->block.statements[i]));
+            }
+            sprintf(result, ast->block.main ? "%s" : "{ %s }", temp);
+            break;
+        case AST_FUNC_DEF:
+            for (AST *p = ast->func_def.params; p != NULL; p = p->func_def_param.next) {
+                char *param_code = generate_code(p);
+                if (strcmp(params, "") == 0) sprintf(params, "%s", param_code);
+                else sprintf(params, "%s, %s", params, param_code);
+            }
+
+            sprintf(result, "%s %s (%s) %s", ast->func_def.return_type,
+                    ast->func_def.name, params, generate_code(ast->func_def.body));
+            break;
+
+        case AST_FUNC_DEF_PARAM:
+            sprintf(result, "%s %s", ast->func_def_param.type, ast->func_def_param.name);
+            break;
+        case AST_FUNC_RETURN:
+            sprintf(result, "return %s;", generate_code(ast->func_return));
+            break;
+        case AST_OPERATOR:
+            sprintf(result, "%s %c %s", generate_code(ast->op.left), ast->op.sign, generate_code(ast->op.right));
+            break;
+        case AST_IDENTIFIER:
+            sprintf(result, "%s", ast->identifier);
+            break;
+        case AST_NUMBER:
+            sprintf(result, "%d", ast->number);
+            break;
     }
 
-    char * source = read_all_file(argv[1]);
+    return result;
+}
 
-    if (source == NULL) return 1;
+int main(int argc, char *argv[])
+{
+    // if (argc < 2) {
+    //     printf("./transpiler file.nell");
+    //     return 1;
+    // }
+    //
+    // char *source = read_all_file(argv[1]);
+    //
+    // if (source == NULL) return 1;
+    //
+    // TokenList tokens = tokenize(source);
+    //
+    // show_token_list(tokens);
+    //
+    // printf("\n");
+    //
+    // AST *ast = parse(tokens);
+    // show_ast(ast, 0);
+
+    char *source = "soma :: (a: int, b: int) -> int { return a + b; }";
 
     TokenList tokens = tokenize(source);
 
@@ -47,6 +102,14 @@ int main(int argc, char *argv[])
 
     AST *ast = parse(tokens);
     show_ast(ast, 0);
+
+    char *code = generate_code(ast);
+
+    printf("\n");
+
+    printf("source: %s\n", source);
+
+    printf("Ccode:  %s\n", code);
 
     return 0;
 }
