f48dc18eb0fcffd89fb034270b20e9a9fea35d32
manel <manelzaum@icloud.com>
2026-04-25 18:58:08 -0300

    reseting parser.h.

--- transpiler/parser.h
+++ transpiler/parser.h
@@ -3,93 +3,25 @@
 
 #include "lexer.h"
 
-#define false 0
-#define true 1
-
 typedef enum {
-    AST_IDENTIFIER,
-    AST_VAR_DEF,
-    AST_CONST_DEF,
-    AST_ASSIGN,
-    AST_NUMBER,
-    AST_BLOCK,
-    AST_FUNC_DEF,
-    AST_FUNC_DEF_PARAM,
-    AST_FUNC_EXEC,
-    AST_FUNC_EXEC_PARAM,
-    AST_FUNC_RETURN,
-    AST_OPERATOR
+    AST_VAR_DEF
 } ASTType;
 
 typedef struct AST {
     ASTType type;
 
     union {
-        int number;
-
-        struct {
-            char sign;
-            struct AST *left;
-            struct AST *right;
-        } op;
-
-        char *identifier;
-
-        struct {
-            char *assignment;
-            struct AST *to;
-            struct AST *value;
-        } assign;
-
         struct {
             char *name;
             char *type;
-            struct AST *value;
-        } field;
-
-        struct {
-            struct AST **statements;
-            int count;
-            int capacity;
-            int main;
-        } block;
-
-        struct AST *func_return;
-
-        struct {
-            char *name;
-            struct AST *params;
-            int param_count;
-            char *return_type;
-            struct AST *body;
-        } func_def;
-
-        struct {
-            char *name;
-            char *type;
-            struct AST *next;
-        } func_def_param;
-
-        struct {
-            char *name;
-            struct AST *params;
-            int param_count;
-        } func_exec;
-
-        struct {
-            char *name;
-            struct AST *expression;
-            struct AST *next;
-        } func_exec_param;
+        } var_def;
     };
 } AST;
 
 typedef struct {
     TokenList list;
     int pos;
-    int in_function;
 } Parser;
-
 
 void show_ast(AST* node, int indent);
 
