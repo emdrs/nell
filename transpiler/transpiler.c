edddcc136282a738bb3a9b22ac0392637b2585a4
manel <manelzaum@icloud.com>
2026-04-26 17:21:12 -0300

    feat: change sprintf to asprintf to prevent satanic errors.

--- transpiler/transpiler.c
+++ transpiler/transpiler.c
@@ -32,14 +32,14 @@ char * read_all_file(char *path)
 char * generate_code(AST *ast)
 {
     if(ast == NULL) return strdup("");
-    char *result = strdup("");
+    char *result = NULL;
 
     switch (ast->type) {
         case AST_VAR_DEF: {
             if(ast->var_def.initialized)
-                sprintf(result, "%s %s", ast->var_def.type, ast->var_def.name);
+                asprintf(&result, "%s %s", ast->var_def.type, ast->var_def.name);
             else
-                sprintf(result, "%s %s;", ast->var_def.type, ast->var_def.name);
+                asprintf(&result, "%s %s;", ast->var_def.type, ast->var_def.name);
             break;
         }
         case AST_CONST_DEF: {
@@ -48,28 +48,43 @@ char * generate_code(AST *ast)
             break;
         }
         case AST_NUMBER: {
-            sprintf(result, "%s", ast->number.value);
+            asprintf(&result, "%s", ast->number.value);
             break;
         }
         case AST_ASSIGN: {
-            sprintf(result, "%s %s %s;", generate_code(ast->assign.left),
-                    ast->assign.type, generate_code(ast->assign.right));
+            char *left_code = generate_code(ast->assign.left);
+            char *right_code = generate_code(ast->assign.left);
+
+            asprintf(&result, "%s %s %s;", left_code, ast->assign.type, right_code);
+
+            free(left_code);
+            free(right_code);
             break;
         }
         case AST_IDENTIFIER: {
-            sprintf(result, "%s", ast->identifier);
+            asprintf(&result, "%s", ast->identifier);
             break;
         }
         case AST_OPERATOR: {
-            sprintf(result, "%s %s %s", generate_code(ast->op.left), ast->op.type,
-                    generate_code(ast->op.right));
+            char *left_code = generate_code(ast->op.left);
+            char *right_code = generate_code(ast->op.left);
+
+            sprintf(result, "%s %s %s", left_code, ast->op.type, right_code);
+
+            free(left_code);
+            free(right_code);
             break;
         }
         case AST_BLOCK: {
             char *code = strdup("");
-            for (int i = 0; i < ast->block.size; i++)
-                sprintf(code, "%s %s", code, generate_code(ast->block.statements[i]));
-            sprintf(result, "{%s }", code);
+            for (int i = 0; i < ast->block.size; i++) {
+                char *statement_code = generate_code(ast->block.statements[i]);
+
+                asprintf(&code, "%s %s", code, statement_code);
+
+                free(statement_code);
+            }
+            asprintf(&result, "{%s }", code);
             break;
         }
     }
